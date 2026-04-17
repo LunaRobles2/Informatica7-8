51e3d07f2b8cba997a98275fe0b8e9458ffb74e9
LunaRobles2 <1lunabisakura@gmail.com>
2026-04-17 17:30:14 +0000

    event

---
+++ js-events/script.js
@@ -0,0 +1,18 @@
+// function greet(name){
+//     console.log("Hello "+name);
+// }
+
+// greet("Luna");
+
+// function addNumbers(num1, num2) {
+//     let sum = num1 + num2;
+//     alert("sum is equal to " +sum);
+// }
+
+//addNumbers(6,7)
+
+
+function Article1(a1){
+    alert(" We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost."+a1);
+}
+Article1="is the Article number one";
