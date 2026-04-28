3be2343638586f82f8b774ae0461afde3ba9d76c
LunaRobles2 <1lunabisakura@gmail.com>
2026-04-28 16:40:41 +0000

    comit

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,14 +1,37 @@
 function coin() {
   let random = Math.random();
+    let text = document.querySelector("h2");
 
-
-let text = document.querySelector("h2");
-
-if (random < 0.5){
+if (random > 0.5){
     text.innerHTML = "Tail";
 }
-
-else (random < 0.5){
+else {
     text.innerHTML = "Head";
 }
+
+}
+
+function coin2() {
+  let random2 = Math.random();
+    let text2 = document.querySelector("h3");
+
+if (random2 > 0.5){
+    text2.innerHTML = "Tail";
+}
+else {
+    text2.innerHTML = "Head";
+}
+
+}
+function coin3() {
+  let random3 = Math.random();
+    let text3 = document.querySelector("h4");
+
+if (random3 > 0.5){
+    text3.innerHTML = "Tail";
+}
+else {
+    text3.innerHTML = "Head";
+}
+
 }
