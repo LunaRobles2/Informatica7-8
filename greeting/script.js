967aea3ab391438cc2ea8beea922d9a7e405c991
LunaRobles2 <1lunabisakura@gmail.com>
2026-05-07 17:30:00 +0000

    day

--- greeting/script.js
+++ greeting/script.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
-document.querySelector("form").onsubmit = function(){
+    document.querySelector("form").onsubmit = function(){
+
     let name = document.querySelector("#name").value;
     console.log("Hi"+name);
 
-    document.querySelector("h2").innerHTML = "Hi "+name;
+    document.querySelector("h1").innerHTML = "Hi "+name;
+
+//////////////////////////////////////////////////////////////////////////////////
+    let now = new Date();
+
+    let hour = now.getHours();
+    if (hour < 12)
+        { document.querySelector("#time").innerHTML = "☀️Good morning!"; }
+    else if (hour < 18)
+        { document.querySelector("#time").innerHTML = "🌄 Good afternoon!"; }
+    else
+        { document.querySelector("#time").innerHTML = "🌜Good night!"; }
+
+//////////////////////////////////////////////////////////////////////////////////
+
+    const dweek = today.toLocaleDateString('en-US', { weekday: 'long' });
+    console.log(dayName);
+
+//////////////////////////////////////////////////////////////////////////////////
 
     return false; //Prevent website from reloading
 };
-})
+});
