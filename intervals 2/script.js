6d256bc5ec5232d333e49bf4dfb3415214d0f7c3
LunaRobles2 <1lunabisakura@gmail.com>
2026-05-13 20:21:42 +0000

    time

--- intervals 2/script.js
+++ intervals 2/script.js
@@ -1,25 +1,23 @@
-let time = choose;
 let alarm = new Audio("alarm.mp3");
+let time;
+document.addEventListener('DOMContentLoaded', function() {
 
-if 
-choose =
-
-
-function count() {
-
-    if (time > 0){
-    time--;
-    document.querySelector('h1').innerHTML = time;}
+    document.querySelector('#m1').onclick = function(){
+        time = 60;
+        if (time > 0){
+        time--;
+        document.querySelector('h1').innerHTML = time;}
 
     else if (time == 0) {
     alarm.play();
     time--;
-}}
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = function(){
     setInterval(count, 1000);
     document.querySelector("button").disabled = true;
     };
 });
+
+
+}}
+
+
