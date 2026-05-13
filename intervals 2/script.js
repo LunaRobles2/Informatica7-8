803e329394d108f33f9d8c1501c40a5ff53f531a
LunaRobles2 <1lunabisakura@gmail.com>
2026-05-13 21:37:33 +0000

    cuac

--- intervals 2/script.js
+++ intervals 2/script.js
@@ -1,16 +1,19 @@
 let alarm = new Audio("alarm.mp3");
-let time;
-document.addEventListener('DOMContentLoaded', function() {
 
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#m1').onclick = function(){
-        time = 60;
-        if (time > 0){
-        time--;
-        document.querySelector('h1').innerHTML = time;}
+    let time = 61;
+    function count() {
+
+    if (time > 0){
+    time--;
+    document.querySelector('h1').innerHTML = time;}
 
     else if (time == 0) {
     alarm.play();
     time--;
+}}
+
 
     setInterval(count, 1000);
     document.querySelector("button").disabled = true;
@@ -18,6 +21,45 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#m5').onclick = function(){
+    let time = 301;
+    function count() {
+
+    if (time > 0){
+    time--;
+    document.querySelector('h1').innerHTML = time;}
+
+    else if (time == 0) {
+    alarm.play();
+    time--;
 }}
 
 
+    setInterval(count, 1000);
+    document.querySelector("button").disabled = true;
+    };
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#m10').onclick = function(){
+    let time = 601;
+    function count() {
+
+    if (time > 0){
+    time--;
+    document.querySelector('h1').innerHTML = time;}
+
+    else if (time == 0) {
+    alarm.play();
+    time--;
+}}
+
+
+    setInterval(count, 1000);
+    document.querySelector("button").disabled = true;
+    };
+});
